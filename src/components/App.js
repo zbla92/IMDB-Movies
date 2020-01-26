74a796ef3be03bcffbdb28b5fc3c149665a77f71
VIVANT <milanblaz@live.com>
2020-01-26 14:06:59 +0100

    fixing issue that caused heroku deployment failure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import GridView from './GridView';
+import GridView from '../pages/GridView';
 import Header from './Header';
 import Footer from '../components/Footer'
-import TableView from './TableView';
+import TableView from '../pages/TableView';
 
 function App() {
   return (
